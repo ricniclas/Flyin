458094c1c99b47a22f4ae1609fc254ee85b0a0cd
Welliton Souza <wellitondeasouza@gmail.com>
2019-06-03 05:02:52 -0300

    Adicionados .py das animações de Cutscenes, Start, Go e Fade in_out

---
+++ FF/Cutscenes.py
@@ -0,0 +1,41 @@
+import pyganim
+
+Cutscene_2 = pyganim.PygAnimation([('images\Tela_Inicial\Cutscene_2.png', 750),
+                                 ('images\Tela_Inicial\Cutscene_2_1.png', 750),
+                                 ('images\Tela_Inicial\Cutscene_2_2.png', 750)])
+
+Cutscene_5 = pyganim.PygAnimation([('images\Tela_Inicial\Cutscene_5_0.png',50),
+                                 ('images\Tela_Inicial\Cutscene_5_1.png', 50),
+                                 ('images\Tela_Inicial\Cutscene_5_2.png', 50),
+                                 ('images\Tela_Inicial\Cutscene_5_3.png', 50),
+                                 ('images\Tela_Inicial\Cutscene_5_4.png', 50)])
+
+Cutscene_Kobra = pyganim.PygAnimation([('images\Tela_Inicial\Cutscene_7_0.png',80),
+                                 ('images\Tela_Inicial\Cutscene_7_1.png', 80),
+                                 ('images\Tela_Inicial\Cutscene_7_2.png', 80),
+                                 ('images\Tela_Inicial\Cutscene_7_3.png', 80),
+                                 ('images\Tela_Inicial\Cutscene_7_4.png', 2000)])
+
+Go = pyganim.PygAnimation([('images\Go.png',200),
+                                 ('images\Go_0.png', 200)])
+
+Start_Play = pyganim.PygAnimation([('images\Start__Play_00.png',60),
+                                 ('images\Start__Play_01.png', 60),
+                                 ('images\Start__Play_02.png', 60),
+                                 ('images\Start__Play_03.png', 60),
+                                 ('images\Start__Play_04.png', 60),
+                                 ('images\Start__Play_05.png', 60),
+                                 ('images\Start__Play_06.png', 60),
+                                 ('images\Start__Play_07.png', 60),
+                                 ('images\Start__Play_08.png', 60),
+                                 ('images\Start__Play_09.png', 300),
+                                 ('images\Start__Play_10.png', 60),
+                                 ('images\Start__Play_11.png', 60),
+                                 ('images\Start__Play_12.png', 60),
+                                 ('images\Start__Play_13.png', 60),
+                                 ('images\Start__Play_14.png', 60),
+                                 ('images\Start__Play_15.png', 60),
+                                 ('images\Start__Play_16.png', 60),
+                                 ('images\Start__Play_17.png', 60),
+                                 ('images\Start__Play_18.png', 60),
+                                 ('images\Start__Play_19.png', 60),])
---
+++ FF/Fade.py
@@ -0,0 +1,19 @@
+import pyganim
+
+Fade_out = pyganim.PygAnimation([('images\Tela_Inicial\Fade__out_0.png', 50),
+                                 ('images\Tela_Inicial\Fade__out_1.png', 50),
+                                 ('images\Tela_Inicial\Fade__out_2.png', 50),
+                                 ('images\Tela_Inicial\Fade__out_3.png', 50),
+                                 ('images\Tela_Inicial\Fade__out_4.png', 50),
+                                 ('images\Tela_Inicial\Fade__out_5.png', 50),
+                                 ('images\Tela_Inicial\Fade__out_6.png', 50),
+                                 ('images\Tela_Inicial\Fade__out_7.png', 50)])
+
+Fade_in = pyganim.PygAnimation([('images\Tela_Inicial\Fade__in_0.png', 50),
+                                 ('images\Tela_Inicial\Fade__in_1.png', 50),
+                                 ('images\Tela_Inicial\Fade__in_2.png', 50),
+                                 ('images\Tela_Inicial\Fade__in_3.png', 50),
+                                 ('images\Tela_Inicial\Fade__in_4.png', 50),
+                                 ('images\Tela_Inicial\Fade__in_5.png', 50),
+                                 ('images\Tela_Inicial\Fade__in_6.png', 50),
+                                 ('images\Tela_Inicial\Fade__in_7.png', 50)])
